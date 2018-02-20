c2eda2650c7f4656d810557b7d536968600a5956
parazyd <parazyd@dyne.org>
2018-02-20 12:15:44 +0100

    Add license headers to the source code files.

--- cmd/dam-dir/main_test.go
+++ cmd/dam-dir/main_test.go
@@ -1,6 +1,24 @@
 package main
 
-// See LICENSE file for copyright and license details.
+/*
+ * Copyright (c) 2017-2018 Dyne.org Foundation
+ * tor-dam is written and maintained by Ivan J. <parazyd@dyne.org>
+ *
+ * This file is part of tor-dam
+ *
+ * This source code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this source code. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import (
 	"encoding/base64"
--- python/damhs.py
+++ python/damhs.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
-# See LICENSE file for copyright and license details.
+# Copyright (c) 2017-2018 Dyne.org Foundation
+# tor-dam is writen and maintained by Ivan J. <parazyd@dyne.org>
+#
+# This file is part of tor-dam
+#
+# This source code is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this source code. If not, see <http://www.gnu.org/licenses/>.
+
 """
 Controller daemon running the ephemeral hidden service.
 
