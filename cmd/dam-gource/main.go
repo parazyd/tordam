8e1bca1527e57fa25eaf037fc2de06b052011fe1
parazyd <parazyd@dyne.org>
2018-10-28 12:40:33 +0100

    Be a little bit more informative in dam-gource.

--- cmd/dam-gource/main.go
+++ cmd/dam-gource/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	lib "github.com/parazyd/tor-dam/pkg/damlib"
 )
@@ -10,9 +11,11 @@ func main() {
 	pubsub := lib.RedisCli.Subscribe(lib.PubSubChan)
 	_, err := pubsub.Receive()
 	lib.CheckError(err)
+	fmt.Fprintf(os.Stderr, "Subscribed to %s channel in Redis\n", lib.PubSubChan)
 
 	ch := pubsub.Channel()
 
+	fmt.Fprintf(os.Stderr, "Listening to messages...\n")
 	for msg := range ch {
 		fmt.Println(msg.Payload)
 	}
