c54f18dda6055010a33303fbf9235a260415bc9b
parazyd <parazyd@dyne.org>
2018-10-31 22:05:28 +0100

    damlib: Move GenRandomASCII to helpers.go.

--- pkg/damlib/helpers.go
+++ pkg/damlib/helpers.go
@@ -23,8 +23,10 @@ package damlib
 import (
 	"bytes"
 	"compress/gzip"
+	"crypto/rand"
 	"encoding/base64"
 	"log"
+	"math/big"
 	"strings"
 )
 
@@ -78,4 +80,24 @@ func ParseDirs(sl []string, data []byte) []string {
 		}
 	}
 	return sl
+}
+
+// GenRandomASCII generates a random ASCII string of a given length.
+// Takes length int as argument, and returns a string of that length on success
+// and error on failure.
+func GenRandomASCII(length int) (string, error) {
+	var res string
+	for {
+		if len(res) >= length {
+			return res, nil
+		}
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(127)))
+		if err != nil {
+			return "", err
+		}
+		n := num.Int64()
+		if n > 32 && n < 127 {
+			res += string(n)
+		}
+	}
 }
