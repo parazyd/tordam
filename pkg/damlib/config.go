6d8fac0b3c2775ae45669b3ed69fc6a113e5388a
parazyd <parazyd@dyne.org>
2018-12-17 09:45:34 -0500

    Apply my full name in the license headers.

--- pkg/damlib/config.go
+++ pkg/damlib/config.go
@@ -2,22 +2,22 @@ package damlib
 
 /*
  * Copyright (c) 2017-2018 Dyne.org Foundation
- * tor-dam is written and maintained by Ivan J. <parazyd@dyne.org>
+ * tor-dam is written and maintained by Ivan Jelincic <parazyd@dyne.org>
  *
  * This file is part of tor-dam
  *
- * This source code is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this source code. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import "os"
